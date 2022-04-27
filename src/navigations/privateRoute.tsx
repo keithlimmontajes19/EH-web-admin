e40f54fe322f725b344f126188be236ab349a7b6
suntoes <mar26xd@gmail.com>
2022-04-27 18:00:05 +0800

    [feature] lms > courses

--- src/navigations/privateRoute.tsx
+++ src/navigations/privateRoute.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom';
 
 import Home from 'views/private/Home';
-import Learn from 'views/private/Learn';
+import Courses from 'views/private/Learn/Courses';
+import Learn from 'views/private/Learn/Learn';
+import Team from 'views/private/Team/Team';
 import Dashboards from 'views/private/Team/Dashboards';
 
 const Navigation = () => {
@@ -10,7 +12,9 @@ const Navigation = () => {
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/home" component={Home} />
-      <Route path="/learn" component={Learn} />
+      <Route exact path="/learn" component={Learn} />
+      <Route path="/learn/courses/:page?/:subpage?" component={Courses} />
+      <Route exact path="/team" component={Team} />
       <Route path="/team/dashboards" component={Dashboards} />
       <Redirect to="/" />
     </Switch>
