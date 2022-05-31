a053007562dd7589eb167a07e851ba6d476d6dd0
Keith Montajes <keithmontajes@Keiths-MacBook-Pro.local>
2022-06-01 03:28:32 +0800

    [feature] onboaring screen integrations'

--- src/ducks/onboarding/actionCreator.ts
+++ src/ducks/onboarding/actionCreator.ts
@@ -1,11 +1,31 @@
-import { TYPES } from './actionTypes'
+import { TYPES } from "./actionTypes";
 
 export const postWebBriefOnboaring = (payload: any) => ({
-    type: TYPES.POST_ONBOARDING_YOU_REQUEST,
-    payload
-})
+  type: TYPES.POST_ONBOARDING_YOU_REQUEST,
+  payload,
+});
 
 export const postCreativeProvider = (payload: any) => ({
-    type: TYPES.POST_CREATIVE_PROVIDER_REQUEST,
-    payload
-})
+  type: TYPES.POST_CREATIVE_PROVIDER_REQUEST,
+  payload,
+});
+
+export const getOnboardingList = () => ({
+  type: TYPES.GET_ONBOARDING_LIST_REQUEST,
+});
+
+export const getOneOnboarding = (payload: any) => ({
+  type: TYPES.GET_ONE_ONBOARDING_REQUEST,
+  payload,
+});
+
+export const deleteOnboading = (payload: any) => ({
+  type: TYPES.DELETE_ONBOARDING_REQUEST,
+  payload,
+});
+
+export const editOnboarding = (payload: any, id) => ({
+  type: TYPES.EDIT_ONBOARDING_REQUEST,
+  payload,
+  id,
+});
--- src/ducks/onboarding/reducer.ts
+++ src/ducks/onboarding/reducer.ts
@@ -1,42 +1,104 @@
-import { TYPES } from './actionTypes';
+import { TYPES } from "./actionTypes";
 
 const INITIAL_STATE = {
   onboarding_you: {
     data: {},
     loading: false,
-    error: false
-  }
+    error: false,
+  },
+  onboarding_list: {
+    data: [],
+    loading: false,
+    error: false,
+  },
+  onboarding: {},
 };
 
 const reducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case TYPES.POST_ONBOARDING_YOU_REQUEST:
       return {
+        ...state,
         onboarding_you: {
           data: {},
           loading: true,
-          error: false
-        }
+          error: false,
+        },
       };
 
     case TYPES.POST_ONBOARDING_YOU_SUCCESS:
       return {
+        ...state,
         onboarding_you: {
           data: action.payload,
           loading: false,
-          error: false
-        }
+          error: false,
+        },
       };
 
     case TYPES.POST_ONBOARDING_YOU_FAILED:
       return {
+        ...state,
         onboarding_you: {
           data: action.payload,
           loading: false,
-          error: true
-        }
+          error: true,
+        },
       };
 
+    /**
+     * ==================
+     * ONBOARDING LIST
+     * ==================
+     */
+
+    case TYPES.GET_ONBOARDING_LIST_REQUEST:
+      return {
+        ...state,
+        onboarding_list: {
+          data: [],
+          loading: true,
+          error: false,
+        },
+      };
+
+    case TYPES.GET_ONBOARDING_LIST_SUCCESS:
+      return {
+        ...state,
+        onboarding_list: {
+          data: action.payload,
+          loading: false,
+          error: false,
+        },
+      };
+
+    case TYPES.GET_ONBOARDING_LIST_FAILED:
+      return {
+        ...state,
+        onboarding_list: {
+          data: [],
+          loading: false,
+          error: true,
+        },
+      };
+
+    /**
+     * ==================
+     * GET ONE ONBOARDING
+     * ==================
+     */
+
+    case TYPES.GET_ONE_ONBOARDING_REQUEST:
+      return {
+        ...state,
+        onboarding: {},
+      };
+
+    case TYPES.GET_ONBOARDING_LIST_SUCCESS:
+      return {
+        ...state,
+        onboarding: action.payload,
+      };
 
     default:
       return { ...state };
