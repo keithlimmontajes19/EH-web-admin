4ca0b551e82a7d7f73866725bd0ae2e1347fb67e
keithlimmontajes <keithlimmontajes@gmail.com>
2022-08-24 00:57:43 +0800

    [hotfix] error in routes

--- src/navigations/publicRoute.tsx
+++ src/navigations/publicRoute.tsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
-import Login from "views/public/Login";
-import EnterOTP from "compositions/EnterOtp";
-import ResetPassword from "compositions/ResetPassword";
-import ForgotPassword from "compositions/ForgotPassword";
+import Login from 'views/public/Login';
+import EnterOTP from 'compositions/EnterOtp';
+import ResetPassword from 'compositions/ResetPassword';
+import ForgotPassword from 'compositions/ForgotPassword';
 
 const Navigation = () => {
   return (
     <Switch>
       <Route exact path="/" component={Login} />
-      <Route exact path="/otp" component={EnterOTP} />
-      <Route exact path="/reset-password" component={ResetPassword} />
-      <Route exact path="/forgot-password" component={ForgotPassword} />
+      {/* <Route exact path="/otp" component={EnterOTP} /> */}
+      {/* <Route exact path="/reset-password" component={ResetPassword} /> */}
+      {/* <Route exact path="/forgot-password" component={ForgotPassword} /> */}
       <Redirect to="/" />
     </Switch>
   );
